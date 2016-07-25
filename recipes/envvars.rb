bbf00cad300c7dbaae9acec5d9b8bdb1b3ee2554
ahmed-ayoub <ahmed.bebo.ayoub@gmail.com>
2016-07-25 10:47:02 +0200

    Adding app variables 2

--- recipes/envvars.rb
+++ recipes/envvars.rb
@@ -1,6 +1,6 @@
 ruby_block  "set-app-var" do
   block do
-    ENV["REDIS_WRITABLE_IP"] = node[:deploy]['simplephpapp'][:environment_variables][:REDIS_WRITABLE_IP]
-    ENV["REDIS_WRITABLE_PORT"] = node[:deploy]['simplephpapp'][:environment_variables][:REDIS_WRITABLE_PORT]
+    ENV["REDIS_WRITABLE_IP"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]
+    ENV["REDIS_WRITABLE_PORT"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]
   end
 end

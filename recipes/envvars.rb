e492a758329fa1e201140fde973786d5231b46b1
ahmed-ayoub <ahmed.bebo.ayoub@gmail.com>
2016-07-25 11:23:47 +0200

    Adding app variables 5

--- recipes/envvars.rb
+++ recipes/envvars.rb
@@ -3,8 +3,8 @@ Chef::Log.info("REDIS_WRITABLE_PORT: #{node[:deploy]['charging_module'][:environ
 
 ruby_block  "set-app-var" do
   block do
-    ENV["REDIS_WRITABLE_IP"] = #{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]}
-    ENV["REDIS_WRITABLE_PORT"] = #{node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]}
+    ENV["REDIS_WRITABLE_IP"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_IP]
+    ENV["REDIS_WRITABLE_PORT"] = node[:deploy]['charging_module'][:environment_variables][:REDIS_WRITABLE_PORT]
   end
 end
 
